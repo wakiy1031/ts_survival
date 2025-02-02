ac9535ee09c58515054d26cecfb1c424d7b7da0d
wakiy1031 <wakiy1031@gmail.com>
2025-02-02 18:15:41 +0900

    Add random cat image generator with TypeScript and Next.js

---
+++ random-cat/pages/index.tsx
@@ -0,0 +1,55 @@
+import { NextPage, GetServerSideProps } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+
+  return (
+    <div className={styles.page}>
+      <button className={styles.button} onClick={handleClick}>
+        他の猫を見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
+};
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
+
+fetchImage().then((image) => {
+  console.log(image.url);
+});
