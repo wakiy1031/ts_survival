f8bf1d1506bf9f9879ec857f8ef1185e62230bd1
wakiy1031 <wakiy1031@gmail.com>
2025-02-02 21:43:40 +0900

    Update button text to English

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -20,7 +20,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button className={styles.button} onClick={handleClick}>
-        他の猫を見る
+        One more cat!
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
